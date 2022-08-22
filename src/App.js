3f6173f07f4993bc0dc08c350c08079e0a6b4951
Ajith178 <ajithnirmal171097@gmail.com>
2022-08-22 15:53:17 +0530

    pending 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import "./App.css"
+import { useEffect, useState } from "react";
+import Reactpaginate from "react-paginate";
+const App = ()=>{
+  const [users,setUsers] = useState([])
+  const [pageNo,setPageNumber] = useState(0)
+  useEffect(()=>{
+    axios({
+      url:"https://fakestoreapi.com/products",
+      method:"GET",
+    }).then((data)=>{
+      setUsers([...users,data.data])
+    }).catch((err)=>{
+      console.log(err)
+    })
+  },[])
+ 
+  const usersPerPage = 10;
+   const pageVisited = pageNo * usersPerPage;
+   const displayUsers = users
+   .slice(pageVisited, pageVisited + usersPerPage)
+   .map((user,i) => {
+    console.log(user[i])
+     return (
+          
+         <img src={user[i].image} width="60px" height="60px"></img>
+         
+         
+  
+     );
+   });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const changePage = ({selected})=>{
+  setPageNumber(selected)
 }
+
+
+const pageCount = Math.ceil(users.length / usersPerPage)
+
+  return (
+   
+  <div className="margin">
+    
+     <h1>Available Product</h1>
+     {displayUsers}
+    <Reactpaginate
+    previousLabel={"previous"}    
+    nextLabel={"next"}
+    pageCount={pageCount}
+    onPageChange={changePage}
+    containerClassName={"paginattionbtn"}
+    previousLinkClassName={"previousBttn"}
+    nextLinkClassName={"nextBttn"}
+    disabledClassName={"paginationDisabled"}
+    activeClassName={"paginationActive"}
+    ></Reactpaginate>
+  </div>)
+
+  
+
+  }
+
 
 export default App;
