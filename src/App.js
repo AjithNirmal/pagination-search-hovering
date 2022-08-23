ac88694f2c21050a7fe01e334e179d585dce15ae
Ajith178 <ajithnirmal171097@gmail.com>
2022-08-23 15:29:01 +0530

    hovering

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 import Reactpaginate from "react-paginate";
 const App = ()=>{
   const [users,setUsers] = useState([])
+  const [desc,setDesc] = useState([])
+  const [show,setShow] = useState(false)
+  const [hover,Sethover] = useState("");
   const [pageNo,setPageNumber] = useState(0)
   useEffect(()=>{
     axios({
@@ -11,19 +14,38 @@ const App = ()=>{
       method:"GET",
     }).then((data)=>{
       setUsers(data.data)
+      setDesc(data.data)
     }).catch((err)=>{
       console.log(err)
     })
   },[])
-  console.log(users)
+  let id = null;
+  const onHover = (e)=>{
+    setShow(true)
+    let no = parseInt(e.target.name)
+    desc.slice(no-1,no).map((items)=>{
+      Sethover(items.description)
+      return(
+      <p>{items.description}</p>
+      )
+    })
+  }
+  const onHoverOver = ()=>{
+    console.log(hover,show)
+    setShow(false)
+    Sethover("");
+  }
   const usersPerPage = 10;
    const pageVisited = pageNo * usersPerPage;
    const displayUsers = users
    .slice(pageVisited, pageVisited + usersPerPage)
    .map((user) => {
-    console.log(user)
      return (
-         <img src={user.image} width="60px" height="60px"></img>
+         <img src={user.image} width="60px" height="60px" name = {user.id}onMouseEnter={(e) => onHover(e)}
+         onMouseLeave={(e) => onHoverOver(e)}  >
+
+          
+         </img>
      );
    });
 
@@ -42,6 +64,7 @@ const pageCount = Math.ceil(users.length / usersPerPage)
     
      <h1>Available Product</h1>
      {displayUsers}
+     {setShow && <h3>{hover}</h3>}
     <Reactpaginate
     previousLabel={"previous"}    
     nextLabel={"next"}
